10415199d3fd989d123b72bc8271b8cccbd9d2c9
choihanna <hnc24@naver.com>
2021-09-16 23:36:28 +0900

    DFS&BFS

--- DFS&BFS/미로탈출게임.cpp
+++ DFS&BFS/미로탈출게임.cpp
@@ -23,22 +23,23 @@ void BFS()
 
     {
         pair<int, int> cur = q.front();
-        int curX = cur.first;
-        int curY = cur.second;
+        int curX = cur.first;  // 인덱스
+        int curY = cur.second; // 해당 인덱스 값
 
         q.pop();
 
         for (int i = 0; i < 4; i++)
-        {
+        { // 현재 위치에서 4가지 방향으로의 위치 확인
             int nx = curX + delta[i][0];
             int ny = curY + delta[i][1];
 
             if (!(0 <= nx && nx < n && 0 <= ny && ny < n))
                 continue;
 
-            if (data[nx][ny] != '1')
-                continue;
+            if (data[nx][ny] != '1') // 1은 이동할 수 있는 칸, 0은 이동할 수 없는 칸
+                continue;            // 0이기 때문에 무시
 
+            // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
             if (v[nx][ny] == 0)
             {
                 v[nx][ny] = v[curX][curY] + 1;
