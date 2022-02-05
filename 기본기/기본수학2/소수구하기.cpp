da0c8e445ff24bd8d002c849ddf17bd039312ad7
choihanna <hnc24@naver.com>
2022-02-06 01:00:00 +0900

    Basic math2

--- 기본기/기본수학2/소수구하기.cpp
+++ 기본기/기본수학2/소수구하기.cpp
@@ -1,44 +1,39 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
-int arr[1000001]; //전역 변수 선언시 자동으로 0으로 초기화.
-
-void findPrimeNum(int m, int n)
-{
-    //0과 1은 소수가 아니기에 1로 체크해준다.
-    arr[0] = 1;
-    arr[1] = 1;
-
-    //n이하이기 때문에 n+1임.
-    for (int i = 2; i < n + 1; i++)
-    {
-        /* 배수의 시작은 4부터이다. 즉, 숫자 3이하까지는 소수에 해당하는 부분이기 때문에 2*i부터 시작해도 상관이 없다.
-		j+=i는 i만큼 더해지는데, 처음에는 2씩, 다음에는 3씩 더해진다. 즉, 배수의 역할을 하고 있다.
-		ex) i가 2이면 2의배수, 3이면 3의 배수의 역할 */
-        for (int j = 2 * i; j < n + 1; j += i)
-        {
-            if (arr[j] == 0)
-            {
-                arr[j] = 1; //배수는 배열에서 1로 체크된다.
-            }
-        }
-    }
-
-    for (int i = m; i < n + 1; i++)
-    {
-        if (arr[i] == 0)
-            cout << i;
-    }
-}
 
 int main()
 {
-    int m, n;
-    cin >> m >> n;
+    int M, N;
+    int rt; // 제곱근 저장하는 변수
+    cin >> M >> N;
 
-    findPrimeNum(m, n);
+    for (int i = M; i <= N; i++)
+    {
+        rt = sqrt(i);
+        if (rt == 1 && i != 1)
+        { // 2, 3인 경우
+            cout << i << '\n';
+            continue;
+        }
+        /* 제곱근을 저장하는 변수 rt는 int형이기 때문에, 검사 중인 i가 1, 2, 3이라면 rt에는 1이 저장될 것이다.
+            2와 3은 소수이기 때문에 출력하지만 1은 소수가 아니기 때문에 if문에 1을 제외시키는 조건을 넣는다.
+            continue를 실행하면 나머지 반복 부분을 실행하지 않고 다음 i로 넘어가 반복문을 실행.*/
 
-    return 0;
+        if (i % 2)
+        { // 홀수일 경우
+            for (int j = 2; j <= rt; j++)
+            {
+                if (!(i % j))
+                    break;
+                if (j == rt)
+                {
+                    cout << i << '\n';
+                }
+            }
+        }
+        /* 검사 중인 i가 짝수라면 2로 나눠지는 수이므로 소수가 아니다.
+        i가 홀수인 경우에만 i의 제곱근인 2부터 rt까지 검사하여, 나누어 떨어지는 인수 j가 있는 경우 소수가 아니라고 판단하여 
+        검사하는 for문을 break로 탈출한다.*/
+    }
 }
