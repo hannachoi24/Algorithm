19c4637c64cb5904438d86954f90bad2867346fe
choihanna <hnc24@naver.com>
2022-02-08 01:00:00 +0900

    Basic math2

--- 기본기/기본수학2/베르트랑 공준.cpp
+++ 기본기/기본수학2/베르트랑 공준.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n, rt, cnt = 0;
+    int n, rt, cnt = 0; // rt는 제곱근 저장하는 변수
 
     while (1)
     {
         cin >> n;
-        if (!n) //0 입력시 종료
+        if (!n) // 0 입력시 종료
             break;
 
         for (int i = n + 1; i <= 2 * n; i++)
         {
             rt = sqrt(i);
             if (rt == 1 && i != 1)
-            { //2,3인 경우
+            { // 2, 3인 경우
                 cnt++;
                 continue;
             }
             if (i % 2)
-            { //홀수일 경우
+            { // 홀수일 경우
                 for (int j = 2; j <= rt; j++)
                 {
                     if (!(i % j))
