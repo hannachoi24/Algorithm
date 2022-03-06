3fa6db3e93f4633c5888e87ef7d3e055f5c9da60
choihanna <hnc24@naver.com>
2022-03-06 16:03:26 +0900

    Queue_18258

---
+++ 기본기/Queue&Deque/큐2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+
+    int n, data;
+    queue<int> q;
+    string str;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+
+        if (str == "push")
+        {
+            cin >> data;
+            q.push(data);
+        }
+        else if (str == "pop")
+        {
+            if (q.size() == 0)
+                cout << -1 << '\n';
+            else
+            {
+                data = q.front();
+                q.pop();
+                cout << data << '\n';
+            }
+        }
+        else if (str == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if (str == "empty")
+        {
+            cout << q.empty() << '\n';
+        }
+        else if (str == "front")
+        {
+            if (q.size() == 0)
+                cout << -1 << '\n';
+            else
+                cout << q.front() << '\n';
+        }
+        else if (str == "back")
+        {
+            if (q.size() == 0)
+                cout << -1 << '\n';
+            else
+                cout << q.back() << '\n';
+        }
+    }
+    return 0;
+}
