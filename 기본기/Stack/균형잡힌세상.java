be89216d0633b728fd2ba1760c50e6b503c331d8
choihanna <hnc24@naver.com>
2022-09-11 23:54:34 +0900

    Stack_4949

---
+++ 기본기/Stack/균형잡힌세상.java
@@ -0,0 +1,60 @@
+package 기본기.Stack;
+
+import java.io.*;
+import java.util.*;
+
+public class 균형잡힌세상 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s;
+
+        while (true) {
+            s = br.readLine();
+
+            if (s.equals(".")) {
+                break;
+            }
+
+            sb.append(solve(s)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static String solve(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i); // i 번째 문자
+
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            }
+
+            else if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+
+            else if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+
+        }
+
+        if (stack.empty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
+}
