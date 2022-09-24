b3694185f7efd0d538360f4e512508466f4efed2
choihanna <hnc24@naver.com>
2022-09-24 22:21:52 +0900

    Simulation_15685

---
+++ 시뮬레이션/드래곤커브.java
@@ -0,0 +1,54 @@
+package 시뮬레이션;
+
+import java.util.*;
+import java.io.*;
+
+public class 드래곤커브 {
+    static int[] dx = { 0, -1, 0, 1 };
+    static int[] dy = { 1, 0, -1, 0 };
+    static boolean[][] map;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        map = new boolean[101][101];
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()); // 시작점
+            int y = Integer.parseInt(st.nextToken()); // 시작점
+            int d = Integer.parseInt(st.nextToken()); // 방향
+            int g = Integer.parseInt(st.nextToken()); // 세대
+            dragon(x, y, d, g);
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (map[i][j] && map[i + 1][j] && map[i][j + 1] && map[i + 1][j + 1])
+                    answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static void dragon(int c, int r, int d, int g) {
+        ArrayList<Integer> dir = new ArrayList<Integer>();
+        dir.add(d);
+        for (int i = 0; i < g; i++) {
+            for (int j = dir.size() - 1; j >= 0; j--) {
+                dir.add((dir.get(j) + 1) % 4);
+            }
+        }
+
+        map[r][c] = true;
+        for (int i = 0; i < dir.size(); i++) {
+            r += dx[dir.get(i)];
+            c += dy[dir.get(i)];
+            map[r][c] = true;
+        }
+    }
+}
