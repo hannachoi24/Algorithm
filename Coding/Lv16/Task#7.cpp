24a7c9a9aa1c141a9f9479f6f4dc914b64546c76
choihanna <hnc24@naver.com>
2023-02-08 17:01:18 +0900

    Lv16_Task

--- Coding/Lv16/Task#7.cpp
+++ Coding/Lv16/Task#7.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+char arr[3][10] = {0};
+
+int isExist(char ch)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (arr[i][j] == ch)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 
 int main()
 {
-
-    string arr;
-
     for (int i = 0; i < 3; i++)
     {
-        cin >> arr;
+        cin >> arr[i];
     }
 
-    if (arr.find("M"))
+    if (isExist('M'))
     {
-        cout << "M이 존재합니다" << '\n';
+        cout << "M이 존재합니다";
     }
-    else if (arr.find("M") == std::string::npos)
-    {
 
-        cout << "M이 존재하지 않습니다" << '\n';
-    }
+    else
+        cout << "M이 존재하지 않습니다";
 
     return 0;
 }
