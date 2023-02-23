bcd6e01f96ff72d7e04fb2d81d290486d258d8b0
choihanna <hnc24@naver.com>
2023-02-24 06:29:15 +0900

    Lv20_Practice7_재귀호출

---
+++ Coding/Lv20_재귀호출1_무한호출막기/Practice7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void abc(int x)
+{
+    if (x == 2)
+    {
+        cout << "#";
+        return;
+    }
+    // cout << x;
+    abc(x + 1);
+    cout << x;
+    int d = 1;
+}
+
+int main()
+{
+    abc(0);
+
+    return 0;
+}
