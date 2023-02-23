e73eada5af488ccfc4741f53a22f69145d17de11
choihanna <hnc24@naver.com>
2023-02-24 06:20:14 +0900

    Lv20_Practice3_for문

---
+++ Coding/Lv20_재귀호출1_무한호출막기/Practice3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/* for문 연습*/
+
+int main()
+{
+
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            cout << i << " " << j << '\n';
+        }
+    }
+
+    return 0;
+}
