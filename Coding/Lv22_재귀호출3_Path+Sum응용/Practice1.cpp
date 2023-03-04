6d5b0d3add7ef4f8cb7baa6586da424a1a168cbf
choihanna <hnc24@naver.com>
2023-03-04 18:18:31 +0900

    Lv22_Practice1_for문

---
+++ Coding/Lv22_재귀호출3_Path+Sum응용/Practice1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v[2][3][4] = {0};
+
+    v[1][0][2] = 7;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                cout << v[i][j][k];
+            }
+            cout << endl;
+        }
+        cout << "======" << endl;
+    }
+    return 0;
+}
