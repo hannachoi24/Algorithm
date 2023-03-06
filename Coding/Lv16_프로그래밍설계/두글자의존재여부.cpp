2ffc4c2d23fb240383743b548b6acdd7ba2d7bb6
choihanna <hnc24@naver.com>
2023-03-06 16:54:00 +0900

    isExist함수_Task#15

--- Coding/Lv16_프로그래밍설계/두글자의존재여부.cpp
+++ Coding/Lv16_프로그래밍설계/두글자의존재여부.cpp
@@ -52,3 +52,64 @@ int main()
 
     return 0;
 }
+
+/* 또 다른 코드
+#include <iostream>
+using namespace std;
+
+int isExist(char arr[2][7], char input)
+{
+    int x, y;
+
+    for (y = 0; y < 2; y++)
+    {
+        for (x = 0; x < 6; x++)
+        {
+            if (arr[y][x] == input)
+            {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    char arr[2][7] =
+    {
+        "A79TKQ",
+        "MINCOD",
+    };
+
+    char input1, input2;
+    int result1, result2;
+
+    cin >> input1 >> input2;
+
+    result1 = isExist(arr, input1);
+    result2 = isExist(arr, input2);
+
+    if (result1 == 1)
+    {
+        cout << input1 << " : 존재" << endl;
+    }
+    else
+    {
+        cout << input1 << " : 없음" << endl;
+
+    }
+
+    if (result2 == 1)
+    {
+        cout << input2 << " : 존재" << endl;
+    }
+    else
+    {
+        cout << input2 << " : 없음" << endl;
+    }
+
+    return 0;
+}
+*/
