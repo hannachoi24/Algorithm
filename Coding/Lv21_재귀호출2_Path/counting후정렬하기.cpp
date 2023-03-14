c4ada62b5905ce0436d4c230facdca79994bdb56
choihanna <hnc24@naver.com>
2023-03-14 15:42:24 +0900

    Direct Address Table_Task#13

---
+++ Coding/Lv21_재귀호출2_Path/counting후정렬하기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int vect[8];
+int bucket[10];
+
+int main()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> vect[i];
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        bucket[vect[i]]++;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < bucket[i]; j++)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
