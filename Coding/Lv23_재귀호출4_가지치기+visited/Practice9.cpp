911a9c6d56993aadb2af8a53a5e2855a052efa52
choihanna <hnc24@naver.com>
2023-03-16 20:45:33 +0900

    Lv23_Practice9_중복순열&방문처리&가지치기

---
+++ Coding/Lv23_재귀호출4_가지치기+visited/Practice9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// 다섯개의 후보 중에 1~3등을 뽑으려고 한다. 단 T는 1등 불가하도록 구현
+
+char word[6] = "QWERT";
+char path[10];
+bool visit[5];
+
+void run(int lev)
+{
+    // if (path[0] == 'T') return; // 가지치기1
+
+    if (lev == 3)
+    {
+        cout << path << '\n';
+        return;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (visit[i] == true)
+            continue;
+        if (lev == 0 && word[i] == 'T') // 가지치기2
+            continue;
+        visit[i] = true;
+        path[lev] = word[i];
+        run(lev + 1);
+        path[lev] = 0;
+        visit[i] = false;
+    }
+}
+
+int main()
+{
+    run(0);
+
+    return 0;
+}
