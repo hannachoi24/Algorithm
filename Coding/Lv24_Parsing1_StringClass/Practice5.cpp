5ec9e42fc2ae7f643c8b1f83cf7e64eb9afb330d
choihanna <hnc24@naver.com>
2023-03-17 22:38:25 +0900

    문자열 이어붙이기_Practice5

---
+++ Coding/Lv24_Parsing1_StringClass/Practice5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a = "ABC";
+    string b = "DEF";
+    string c = a + b;
+
+    cout << c;
+
+    return 0;
+}
