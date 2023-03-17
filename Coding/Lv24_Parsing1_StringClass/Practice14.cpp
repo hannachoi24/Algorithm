036f8c4d41db477d3527ae517db6f1a831f23786
choihanna <hnc24@naver.com>
2023-03-17 22:48:06 +0900

    숫자 -> 문자/ 문자 -> 숫자 변환_Practice14

---
+++ Coding/Lv24_Parsing1_StringClass/Practice14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 숫자 -> 문자/ 문자 -> 숫자 변환
+int main()
+{
+    string str = "2324";
+
+    int i = stoi(st);
+
+    cout << i;
+
+    int n = 1234;
+    string a = to_string(n);
+
+    return 0;
+}
