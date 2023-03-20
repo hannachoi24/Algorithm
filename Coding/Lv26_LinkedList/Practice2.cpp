e405ccd9266561131f2ffa7c602ac35000ff9e8d
choihanna <hnc24@naver.com>
2023-03-20 23:15:23 +0900

    구조체_Practice2

---
+++ Coding/Lv26_LinkedList/Practice2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    char ch;
+    int vect[3];
+};
+
+int main()
+{
+    KFC a = {'A', {3, 4, 5}};
+    KFC b = {'E', {3, 4, 1}};
+
+    return 0;
+}
