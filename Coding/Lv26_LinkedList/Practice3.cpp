dcddb05b7c57c11a465f2b4dd82f8988ef31b826
choihanna <hnc24@naver.com>
2023-03-20 23:15:45 +0900

    구조체_Practice3

---
+++ Coding/Lv26_LinkedList/Practice3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+static Node
+{
+    int n;
+    char a, b;
+};
+
+int main()
+{
+    KFC t1 = {3, 'A', 'Q'};
+    KFC t2 = {9, 'T', 'K'};
+
+    return 0;
+}
