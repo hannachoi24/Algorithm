2f902696f8b3e61d38e6ce3615880506c970b685
choihanna <hnc24@naver.com>
2023-03-21 23:38:24 +0900

    vector라이브러리_Practice17

---
+++ Coding/Lv26_LinkedList/Practice17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 배열의 업그레이판 vector라이브러리 사용
+
+int main() {
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(5);
+    v.push_back(7);
+
+    return 0;
+}
