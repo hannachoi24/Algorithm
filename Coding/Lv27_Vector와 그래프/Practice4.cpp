416bfceb49b3df33f0421897d661f888a2aae3e9
choihanna <hnc24@naver.com>
2023-03-22 22:38:11 +0900

    vector_Practice4

---
+++ Coding/Lv27_Vector와 그래프/Practice4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr[3] = {{1, 2, 3}, {4, 5}, {7, 4, 1, 2}};
+
+    cout << arr[0][0];
+
+    return 0;
+}
