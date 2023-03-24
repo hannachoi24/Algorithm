ed290b17d712f95940dadc9ea886451d869e0bb2
choihanna <hnc24@naver.com>
2023-03-25 00:09:39 +0900

    2차원 vector_Practice7

---
+++ Coding/Lv28_Tree와 DFS/Practice7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> alist(4);
+
+    alist[1] = {0, 3};
+    alist[2] = {1, 3};
+
+    return 0;
+}
