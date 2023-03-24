8e0cec156ca3fc8b9f2c52818612dbb128875e2d
choihanna <hnc24@naver.com>
2023-03-25 00:08:32 +0900

    2차원 vector_Practice5

---
+++ Coding/Lv28_Tree와 DFS/Practice5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> v(4);
+
+int main()
+{
+    v[0] = {4, 2, 5, 1, 1};
+    v[1] = {3, 4, 2};
+    v[3] = {1, 1, 2, 3};
+
+    return 0;
+}
