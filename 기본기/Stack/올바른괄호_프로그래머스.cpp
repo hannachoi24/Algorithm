5829c8787a1565a9b9c8cb9c5ba3217edb825a05
choihanna <hnc24@naver.com>
2023-05-06 23:28:37 +0900

    Stack_올바른 괄호_프로그래머스

---
+++ 기본기/Stack/올바른괄호_프로그래머스.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            st.push('(');
+        }
+        else if (!st.empty() && st.top() == '(')
+            st.pop();
+        else
+            st.push(')');
+    }
+
+    if (st.empty())
+    {
+        return true;
+    }
+    else
+        return false;
+}
